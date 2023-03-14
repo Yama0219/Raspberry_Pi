ea72c4869cf40757a305e5dfc12619aa6a744b3e
Yama0219 <cairnhill.sg1109@gmail.com>
2023-03-14 23:25:11 +0900

    remove \n

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 
 TOKEN = ""
 with open(r"token.txt", "r") as f:
-    TOKEN = f.read().splitlines()
+    TOKEN = f.read().replace("\n", "")
 
 API = 'https://notify-api.line.me/api/notify'
 
