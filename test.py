9269e8a433785587c5c59632d85dbe59dfa4d444
Yama0219 <cairnhill.sg1109@gmail.com>
2023-03-14 23:16:23 +0900

    add Token.txt

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import requests
 
 TOKEN = ""
-with open("token.txt", "r") as f:
+with open(r"token.txt", "r") as f:
     TOKEN = f.read()
-    
+
 API = 'https://notify-api.line.me/api/notify'
 
 message = "test"
