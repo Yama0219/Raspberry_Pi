dc2ab887fa099280c314e863e2fdb0a5a40249a4
Yama0219 <cairnhill.sg1109@gmail.com>
2023-03-15 01:40:23 +0900

    update message

--- Line/test.py
+++ Line/test.py
@@ -1,16 +1,21 @@
 import requests
 
 TOKEN = ""
+API = 'https://notify-api.line.me/api/notify'
+
 with open(r"token.txt", "r") as f:
     TOKEN = f.read().replace("\n", "")
 
-API = 'https://notify-api.line.me/api/notify'
+
+def send_message(message):
+    headers = {'Authorization': 'Bearer ' + TOKEN}
+    data = {'message': message}
+    requests.post(API, headers = headers, data = data)
 
 with open("../GPA/GPA.csv", "r") as f:
-    message = f.read()
-
+    GPA = f.read().split()
+    send_message("成績を送るよ！")
+    for message in GPA:
+        send_message(message)
 # message = "raspberry piから送信してるよ！"
 
-headers = {'Authorization': 'Bearer ' + TOKEN}
-data = {'message': message}
-requests.post(API, headers = headers, data = data)
