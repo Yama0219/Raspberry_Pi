f62c8ac2e2dfc2c08395b768b183e1e92c274e72
Yama0219 <cairnhill.sg1109@gmail.com>
2023-03-18 00:43:32 +0900

    update

--- discord/discordbot.py
+++ discord/discordbot.py
@@ -36,6 +36,8 @@ async def on_message(message):
         if message.content.startswith("/command"):
             command = message.content.split()[1:]
             await message.channel.send(f"{' '.join(command)}を実行するよ")
+            output = subprocess.Popen(command)
+            await message.channel.send(output.communicate())
 
     if message.content == '/hello':
         await message.channel.send('やあ，{}さん'.format(message.author))
